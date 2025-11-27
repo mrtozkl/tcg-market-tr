f9b7c96bb9c138089076d199ff6e0173a388c63d
mrtozkl <murat.ozkl@gmail.com>
2025-11-27 14:14:03 +0300

    chore: ignore `server_prod.log` and set `app/page.tsx` to `force-dynamic` rendering

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import Header from '@/components/Header';
 
 import Footer from '@/components/Footer';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home({
     searchParams,
 }: {
